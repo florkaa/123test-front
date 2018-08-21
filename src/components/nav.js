5391aeeebbc80bbe390df92d81ce499b1819bb7c
florkaa <florencia.ayc@gmail.com>
2018-08-21 09:04:38 -0300

    fixes

--- src/components/nav.js
+++ src/components/nav.js
@@ -3,14 +3,32 @@ import Scroll from 'react-scroll-to-element';
 import burger from '../img/line-menu.png'
 
 class Nav extends Component {
+
+	constructor() {
+		super();
+		this.state = {
+			shown: true,
+		};
+	}	
+
+	toggle() {
+		const links = document.getElementById('links');
+		this.state.shown === true 
+			? links.style.display = "none" 
+			: links.style.display = "block";
+
+		this.setState({
+			shown: !this.state.shown
+		});
+	}
+
 	displayMenu = () =>{
 		const links = document.getElementById('links')
-		if (links.style.display === "none") {
-	        links.style.display = "block";
-	    } else {
-	        links.style.display = "none";
-	    }
+		links.style.display === "none" 
+			? links.style.display="block" 
+			: links.style.display="none"
 	}
+
 	render() {
 		if (window.matchMedia("(min-width: 690px)").matches) {
 		    return ( 
@@ -39,9 +57,9 @@ class Nav extends Component {
 				      	<button className="button menu-button" onClick={this.displayMenu}> <img src={burger} alt="menu"/> </button>
 				      	<section id="links" className="links-mobile" style={{'display': 'none'}}>
 				      		<a href="/">Start</a>
-				      		<a href="#about-me">About me</a>
-				      		<a href="#work">Work</a>
-				      		<a href="#contact">Contact</a>
+				      		<a href="#about-me" onClick={this.toggle.bind(this)}>About me</a>
+				      		<a href="#work" onClick={this.toggle.bind(this)}>Work</a>
+				      		<a href="#contact" onClick={this.toggle.bind(this)}>Contact</a>
 				      	</section>
 			      	</section>
 		      	</nav>
